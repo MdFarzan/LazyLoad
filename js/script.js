61a1d9eadaa92dec405ead35c4e772755e65a09b
MdFarzan <mdfarzan776@gmail.com>
2022-09-13 18:47:47 +0530

    imgs lazy-loading feature added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-console.info("script loaded");
+
+const imgs = Array.from(document.images);
+
+function lazyLoad(imgs, observer){
+    imgs.forEach((img)=>{
+        if(img.isIntersecting){
+            let src = img.target.dataset.src;
+            img.target.src = src;
+            observer.unobserve(img.target);
+        }
+        
+    });
+}
+
+const observer = new IntersectionObserver(lazyLoad, {
+    root:null,
+    rootMargin: '0px',
+    threshold: 0.5
+});
+
+imgs.forEach((img)=>{
+    observer.observe(img);
+})
